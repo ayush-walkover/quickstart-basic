988606374444bd13a464eec0321ef8b56468d0c0
ayush-walkover <79703316+ayush-walkover@users.noreply.github.com>
2021-06-09 10:34:02 +0530

    Update AfterInstall.sh

--- Scripts/AfterInstall.sh
+++ Scripts/AfterInstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Set permissions to storage and bootstrap cache
-sudo chmod -R 0777 /home/ec2-user/storage
-sudo chmod -R 0777/home/ec2-user/bootstrap/cache
+sudo chmod -R 0777 /var/www/html/storage
+sudo chmod -R 0777 /var/www/html/bootstrap/cache
 #
 # cp .env.example .env
 # Run composer
