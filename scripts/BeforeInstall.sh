b12afec51e74c990a404fa39ffeb57b25dffd685
ayush-walkover <79703316+ayush-walkover@users.noreply.github.com>
2021-06-09 12:39:37 +0530

    Check file removal

--- scripts/BeforeInstall.sh
+++ scripts/BeforeInstall.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+file="/etc/nginx/sites-enabled/default"
+
+if [ -f "$file" ] ; then
+    rm "$file"
+fi
 curl -sS https://getcomposer.org/installer | php
 sudo mv composer.phar /usr/bin/composer
 sudo chmod +x /usr/bin/composer
